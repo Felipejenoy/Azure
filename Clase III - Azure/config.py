e720b9f3972b4436a461054092246aa9b97c9db6
Felipejenoy <johanordonez@unicauca.edu.co>
2024-10-28 12:48:22 -0500

    corregi mi bd

--- Clase III - Azure/config.py
+++ Clase III - Azure/config.py
@@ -1,8 +1,8 @@
 # Configuración de la cadena de conexión
-SERVER = 'appweb-crud-db-server.database.windows.net'
-DATABASE = 'WebApp-Crud-db'
-USERNAME = 'crud_paas'
-PASSWORD = 'U12345678-'
+SERVER = 'felipe.database.windows.net'
+DATABASE = 'Felipe'
+USERNAME = 'felipe'
+PASSWORD = 'johan.2020'
 DRIVER = 'ODBC Driver 18 for SQL Server'
 
 #connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
